a5f0486dc527be8ba107269d28dffe3d9b4240e7
ChiJian28 <chijianlim2004@gmail.com>
2026-05-12 01:09:38 +0800

    Ignore login and logout

--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,7 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import Login from './components/Login';
-import WelcomeAnimation from './components/WelcomeAnimation';
 import MainApp from './components/MainApp';
 
 export default function Home() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [showWelcome, setShowWelcome] = useState(false);
-  const [welcomeDone, setWelcomeDone] = useState(false);
-  
-  // 1. Add a loading state to prevent the login flash
-  const [isCheckingAuth, setIsCheckingAuth] = useState(true);
-
-  useEffect(() => {
-    const logged = localStorage.getItem('slayyyql_loggedIn');
-    const done = localStorage.getItem('welcomeDone');
-    
-    if (logged === 'true') {
-      setIsLoggedIn(true);
-      if (done !== 'true') {
-        setShowWelcome(true);
-      } else {
-        setWelcomeDone(true);
-        setShowWelcome(false);
-      }
-    }
-    
-    // 2. We are done checking local storage
-    setIsCheckingAuth(false);
-  }, []);
-
-  const handleLoginSuccess = () => {
-    localStorage.setItem('slayyyql_loggedIn', 'true');
-    localStorage.removeItem('welcomeDone');
-    setIsLoggedIn(true);
-    setShowWelcome(true);
-  };
-
-  const handleWelcomeComplete = () => {
-    localStorage.setItem('welcomeDone', 'true');
-    setShowWelcome(false);
-    setWelcomeDone(true);
-  };
-
-  // // 3. Return a blank screen (matching your background) while checking auth
-  // if (isCheckingAuth) {
-  //   return <div className="h-screen w-screen bg-[#060b14]" />;
-  // }
-
-  // if (!isLoggedIn) {
-  //   return <Login onLogin={handleLoginSuccess} />;
-  // }
-
-  // if (showWelcome && !welcomeDone) {
-  //   return <WelcomeAnimation onComplete={handleWelcomeComplete} />;
-  // }
-  <WelcomeAnimation onComplete={handleWelcomeComplete} />
-
   return <MainApp />;
 }
